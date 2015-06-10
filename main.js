49e3f91e8a2fa526a812beb7a4282953caaeb8cb
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-10 21:13:40 +0200

    Improve main.js

--- main.js
+++ main.js
@@ -2,11 +2,31 @@ require.config({
     paths: {
         'app': 'js/app',
         'components': 'js/components',
-        'services': 'js/services'
+        'services': 'js/services',
+        // Plugins
+        'text': 'js/plugins/text',
+        // Vendor
+        'jquery': 'node_modules/jquery/dist/jquery.min',
+        'materialize': 'node_modules/materialize-css/bin/materialize',
+        // lodash
+        '_.isUndefined': 'node_modules/lodash/lang/isUndefined'
+    },
+    shim: {
+        // Lodash functions
+        '_.isUndefined': {
+            exports: '_isUndef'
+        }
     }
 });
 
-require(['app', 'components', 'services'], function(app, components, services) {
-    'use strict';
-    app.init();
-});
+require(['app',
+        'components',
+        'services',
+        'text',
+        'jquery',
+        '_.isUndefined'
+    ],
+    function(app) {
+        'use strict';
+        app.init();
+    });
