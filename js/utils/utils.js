1dcf7a6c2188d53ab4983cb5d0f2241e816953c4
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-11 22:15:20 +0200

    Implemented utils.js

---
+++ js/utils/utils.js
@@ -0,0 +1,11 @@
+define(function() {
+    'use strict';
+
+    var _isUndefined = function(value) {
+        return value === undefined;
+    };
+
+    return {
+        _isUnd: _isUndefined
+    };
+});
