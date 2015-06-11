dffc000da7b43b1b7374bf9aeae35f08f433aef0
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-11 22:56:15 +0200

    created principalContent module

---
+++ js/principalContent.js
@@ -0,0 +1,7 @@
+define(['text!../views/principalContent.html', 'utils'], function(html, utils) {
+    'use strict';
+
+    // ===================================
+    // PRINCIPAL CONTENT
+    // ===================================
+});
