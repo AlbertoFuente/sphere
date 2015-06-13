32987fb8580c9b2240c793b5f05c1722c30ff0f0
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-13 10:52:26 +0200

    principalContent changes

--- js/principalContent.js
+++ js/principalContent.js
@@ -4,4 +4,14 @@ define(['text!../views/principalContent.html', 'utils'], function(html, utils) {
     // ===================================
     // PRINCIPAL CONTENT
     // ===================================
+
+    var _pContent = function(posts) {
+        var container = document.getElementById('postContent');
+
+        console.log(posts);
+    };
+
+    return {
+        pContent: _pContent
+    };
 });
