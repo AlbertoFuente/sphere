1e7ae223c7b557272854b6e530c1996863c4f4ed
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-14 10:30:21 +0200

    Added all rss references, now printing echoJS content

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 define(['topBar', 'principalContent', 'services'], function(topBar, pCont, services) {
     'use strict';
 
+    var infoWorld = 'http://www.infoworld.com/news/index.rss',
+        echoJS = 'http://www.echojs.com/rss',
+        dailyJS = 'http://dailyjs.com/rss/',
+        fiveJS = 'https://fivejs.codeschool.com/feed.rss',
+        jsRocks = 'http://jsrocks.org/rss',
+        frontEndLabs = 'http://frontendlabs.io/feed';
+
     var _init = function() {
         var menuOptions = {
             op1: 'Videos',
@@ -11,9 +18,8 @@ define(['topBar', 'principalContent', 'services'], function(topBar, pCont, servi
         topBar.upMenu('Sphere');
         topBar.upMenu.prototype.insertTitle();
         topBar.upMenu.prototype.insertMenu(menuOptions);
-        // post content
-        //services.configCall(pCont.pContent);
-        services.parseRSS('http://www.echojs.com/rss', pCont.pContent);
+        // post content (echoJS)
+        services.parseRSS(echoJS, pCont.echoContent);
     };
 
     return {
