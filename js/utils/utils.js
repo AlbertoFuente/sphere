f2cb41734c42568f1766ee3abaf246c4f613ea18
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-14 12:44:33 +0200

    Implemented new function to improve appendChild with various elements

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -10,9 +10,21 @@ define(function() {
         elem.setAttribute(attr, val);
         return elem;
     };
+    // APPENDCHILD ARRAY OF ELEMENTS (in order)
+    var _appendArray = function(parent, elements) {
+        if (!_isUndefined(parent) && !_isUndefined(elements) && elements.length > 0) {
+            var i = 0,
+                elLength = elements.length;
+            for (i; i < elLength; i++) {
+                parent.appendChild(elements[i]);
+            }
+        }
+        return parent;
+    };
 
     return {
         _isUnd: _isUndefined,
-        _setAttr: _setAttribute
+        _setAttr: _setAttribute,
+        _appendArr: _appendArray
     };
 });
