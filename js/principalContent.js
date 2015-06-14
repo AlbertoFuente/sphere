6e008a9cce18d871527be6f5440f46dd2f8c4010
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-14 10:30:36 +0200

    Working in principalContent.js

--- js/principalContent.js
+++ js/principalContent.js
@@ -5,13 +5,43 @@ define(['text!../views/principalContent.html', 'utils'], function(html, utils) {
     // PRINCIPAL CONTENT
     // ===================================
 
-    var _pContent = function(posts) {
+    var _echoContent = function(posts) {
         var container = document.getElementById('postContent');
 
-        console.log(posts);
+        if (!utils._isUnd(posts) && !utils._isUnd(html)) {
+            container.innerHTML = html;
+            var miniPanel = document.getElementById('postContent');
+
+            if (!utils._isUnd(miniPanel)) {
+                var title = document.createElement('h5'),
+                    link = document.createElement('a'),
+                    postsContainer = document.createElement('div');
+
+                title.innerHTML = posts.title;
+                link.setAttribute('href', posts.link + '.html');
+                link.innerHTML = posts.link;
+
+                if (!utils._isUnd(posts.entries) && posts.entries.length > 0) {
+                    var i = 0,
+                        entries = posts.entries.length;
+                    for (i; i < entries; i++) {
+                        var subDiv = document.createElement('div'),
+                            subLink = document.createElement('a');
+                        subLink.setAttribute('href', posts.entries[i].link);
+                        subLink.innerHTML = posts.entries[i].title;
+
+                        subDiv.appendChild(subLink);
+                        postsContainer.appendChild(subDiv);
+                    }
+                }
+                miniPanel.appendChild(title);
+                miniPanel.appendChild(link);
+                miniPanel.appendChild(postsContainer);
+            }
+        }
     };
 
     return {
-        pContent: _pContent
+        echoContent: _echoContent
     };
 });
