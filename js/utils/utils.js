89350d6a04229a5a00c1d4c969a372b2ef628a93
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-14 10:41:16 +0200

    Added new function to utils

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,11 +1,18 @@
 define(function() {
     'use strict';
 
+    // UNDEFINED
     var _isUndefined = function(value) {
         return value === undefined;
     };
+    // SET ATTRIBUTE
+    var _setAttribute = function(elem, attr, val) {
+        elem.setAttribute(attr, val);
+        return elem;
+    };
 
     return {
-        _isUnd: _isUndefined
+        _isUnd: _isUndefined,
+        _setAttr: _setAttribute
     };
 });
