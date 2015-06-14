ff5eb72377ba0c55634ebf2cd04c9e8b98b2d9a9
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-14 13:37:54 +0200

    Improve principalContent

--- js/principalContent.js
+++ js/principalContent.js
@@ -1,4 +1,4 @@
-define(['text!../views/principalContent.html', 'utils'], function(html, utils) {
+define(['text!../views/principalContent.html', 'utils', 'jquery'], function(html, utils, $) {
     'use strict';
 
     // ===================================
@@ -10,12 +10,14 @@ define(['text!../views/principalContent.html', 'utils'], function(html, utils) {
 
         if (!utils._isUnd(posts) && !utils._isUnd(html)) {
             container.innerHTML = html;
-            var miniPanel = document.getElementById('postContent');
+            var panel = document.getElementById('postContent'),
+                miniPanel = document.getElementById('echoMiniPanel');
 
             if (!utils._isUnd(miniPanel)) {
                 var title = document.createElement('h5'),
                     link = document.createElement('a'),
-                    postsContainer = document.createElement('div');
+                    postsContainer = document.createElement('div'),
+                    pContainer = document.getElementById('echoContentPanel');
 
                 title.innerHTML = posts.title;
                 utils._setAttr(link, 'href', posts.link + '.html');
@@ -27,7 +29,9 @@ define(['text!../views/principalContent.html', 'utils'], function(html, utils) {
                     for (i; i < entries; i++) {
                         var subDiv = document.createElement('div'),
                             subLink = document.createElement('a');
-                        utils._setAttr(subLink, 'href', posts.entries[i].link);
+                        utils._setAttr(subLink, 'href', '#');
+                        utils._setAttr(subLink, 'data', posts.entries[i].link);
+                        subLink.className = 'subMenuLink';
                         subLink.innerHTML = posts.entries[i].title;
 
                         subDiv.appendChild(subLink);
@@ -35,6 +39,14 @@ define(['text!../views/principalContent.html', 'utils'], function(html, utils) {
                     }
                 }
                 utils._appendArr(miniPanel, [title, link, postsContainer]);
+                panel.appendChild(miniPanel);
+            }
+
+            if (!utils._isUnd(document.getElementsByClassName('subMenuLink'))) {
+                $('.subMenuLink').click(function(ev) {
+                    var link = ev.currentTarget.attributes.data.value;
+                    pContainer.innerHTML = '<object type="text/html" data="' + link + '" ></object>';
+                });
             }
         }
     };
