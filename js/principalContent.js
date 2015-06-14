0a6625dfafc460c7065d58adf464b6eac8bccc23
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-14 20:58:12 +0200

    Improve principalContent.js

--- js/principalContent.js
+++ js/principalContent.js
@@ -5,51 +5,69 @@ define(['text!../views/principalContent.html', 'utils', 'jquery'], function(html
     // PRINCIPAL CONTENT
     // ===================================
 
-    var _echoContent = function(posts) {
-        var container = document.getElementById('postContent');
+    var _appendContent = function(elem, link) {
+            elem.innerHTML = '<object type="text/html" data="' + link + '" ></object>';
+            return elem;
+        },
+        _echoContent = function(posts) {
+            var container = document.getElementById('postContent');
 
-        if (!utils._isUnd(posts) && !utils._isUnd(html)) {
-            container.innerHTML = html;
-            var panel = document.getElementById('postContent'),
-                miniPanel = document.getElementById('echoMiniPanel');
+            if (!utils._isUnd(posts) && !utils._isUnd(html)) {
+                container.innerHTML = html;
+                var panel = document.getElementById('postContent'),
+                    miniPanel = document.getElementById('echoMiniPanel');
 
-            if (!utils._isUnd(miniPanel)) {
-                var title = document.createElement('h5'),
-                    link = document.createElement('a'),
-                    postsContainer = document.createElement('div'),
-                    pContainer = document.getElementById('echoContentPanel');
+                if (!utils._isUnd(miniPanel)) {
+                    var title = document.createElement('h5'),
+                        link = document.createElement('a'),
+                        postsContainer = document.createElement('div'),
+                        pContainer = document.getElementById('echoContentPanel');
 
-                title.innerHTML = posts.title;
-                utils._setAttr(link, 'href', posts.link + '.html');
-                link.innerHTML = posts.link;
+                    title.innerHTML = posts.title;
+                    utils._setAttr(link, 'href', posts.link + '.html');
+                    link.innerHTML = posts.link;
+                    postsContainer.className = 'postMenu';
 
-                if (!utils._isUnd(posts.entries) && posts.entries.length > 0) {
-                    var i = 0,
-                        entries = posts.entries.length;
-                    for (i; i < entries; i++) {
-                        var subDiv = document.createElement('div'),
-                            subLink = document.createElement('a');
-                        utils._setAttr(subLink, 'href', '#');
-                        utils._setAttr(subLink, 'data', posts.entries[i].link);
-                        subLink.className = 'subMenuLink';
-                        subLink.innerHTML = posts.entries[i].title;
+                    if (!utils._isUnd(posts.entries) && posts.entries.length > 0) {
+                        var i = 0,
+                            entries = posts.entries.length;
+                        for (i; i < entries; i++) {
+                            var subDiv = document.createElement('div'),
+                                subLink = document.createElement('a');
+                            utils._setAttr(subLink, 'href', '#');
+                            utils._setAttr(subLink, 'data', posts.entries[i].link);
+                            subLink.className = 'subMenuLink';
+                            subLink.innerHTML = posts.entries[i].title;
 
-                        subDiv.appendChild(subLink);
-                        postsContainer.appendChild(subDiv);
+                            subDiv.appendChild(subLink);
+                            postsContainer.appendChild(subDiv);
+                        }
+                    }
+                    utils._appendArr(miniPanel, [title, link, postsContainer]);
+                    panel.appendChild(miniPanel);
+
+                    if (miniPanel.childNodes.length > 0) {
+                        var j = 0,
+                            minLength = miniPanel.childNodes.length,
+                            firstNode = null;
+                        for (j; j < minLength; j++) {
+                            if (typeof miniPanel.childNodes[j] === 'object' && miniPanel.childNodes[j].tagName === 'DIV') {
+                                firstNode = miniPanel.childNodes[j].childNodes[0].childNodes[0].getAttribute('data');
+                            }
+                        }
+                        _appendContent(pContainer, firstNode);
                     }
                 }
-                utils._appendArr(miniPanel, [title, link, postsContainer]);
-                panel.appendChild(miniPanel);
-            }
 
-            if (!utils._isUnd(document.getElementsByClassName('subMenuLink'))) {
-                $('.subMenuLink').click(function(ev) {
-                    var link = ev.currentTarget.attributes.data.value;
-                    pContainer.innerHTML = '<object type="text/html" data="' + link + '" ></object>';
-                });
+                if (!utils._isUnd(document.getElementsByClassName('subMenuLink'))) {
+                    $('.subMenuLink').click(function(ev) {
+                        $('#echoContentPanel').empty();
+                        var link = ev.currentTarget.attributes.data.value;
+                        _appendContent(pContainer, link);
+                    });
+                }
             }
-        }
-    };
+        };
 
     return {
         echoContent: _echoContent
