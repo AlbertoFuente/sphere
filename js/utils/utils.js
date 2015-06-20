4ea78ce3168c2b5d3f7bf399043e39a9ed57fe27
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-20 23:07:45 +0200

    Improve utils.js

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -3,34 +3,46 @@ define(function() {
 
     // UNDEFINED
     var _isUndefined = function(value) {
-        return value === undefined;
-    };
-    // SET ATTRIBUTE
-    var _setAttribute = function(elem, attr, val) {
-        elem.setAttribute(attr, val);
-        return elem;
-    };
-    // APPENDCHILD ARRAY OF ELEMENTS (in order)
-    var _appendArray = function(parent, elements) {
-        if (!_isUndefined(parent) && !_isUndefined(elements) && elements.length > 0) {
-            var i = 0,
-                elLength = elements.length;
-            for (i; i < elLength; i++) {
-                parent.appendChild(elements[i]);
+            return value === undefined;
+        },
+        // SET ATTRIBUTE
+        _setAttribute = function(elem, attr, val) {
+            elem.setAttribute(attr, val);
+            return elem;
+        },
+        // APPENDCHILD ARRAY OF ELEMENTS (in order)
+        _appendArray = function(parent, elements) {
+            if (!_isUndefined(parent) && !_isUndefined(elements) && elements.length > 0) {
+                var i = 0,
+                    elLength = elements.length;
+                for (i; i < elLength; i++) {
+                    parent.appendChild(elements[i]);
+                }
             }
-        }
-        return parent;
-    };
-    // insert iframe
-    var _appendContent = function(elem, link) {
-        elem.innerHTML = '<object type="text/html" data="' + link + '" id="ifrmObject"></object>';
-        return elem;
-    };
+            return parent;
+        },
+        // insert iframe
+        _appendContent = function(elem, link) {
+            elem.innerHTML = '<object type="text/html" data="' + link + '" id="ifrmObject"></object>';
+            return elem;
+        },
+        // Empty menu container
+        _emptyMenuConteiner = function() {
+            var miniPanel = $('#echoMiniPanel');
+            miniPanel.empty();
+        },
+        // Empty content container
+        _emptyContentContainer = function() {
+            var contentPanel = $('#echoContentPanel');
+            contentPanel.empty();
+        };
 
     return {
         _isUnd: _isUndefined,
         _setAttr: _setAttribute,
         _appendArr: _appendArray,
-        _appendContent: _appendContent
+        _appendContent: _appendContent,
+        _emptyMenuConteiner: _emptyMenuConteiner,
+        _emptyContentContainer: _emptyContentContainer
     };
 });
