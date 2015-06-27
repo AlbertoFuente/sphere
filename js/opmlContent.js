13557cfc7ce1126344cf65d7a735e74f35b49d6c
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-27 08:18:49 +0200

    Created opmlContent.js

---
+++ js/opmlContent.js
@@ -0,0 +1,11 @@
+define(['jquery', 'utils'], function($, utils) {
+    'use strict';
+
+    var _opmlContent = function(posts) {
+        console.log(posts);
+    };
+
+    return {
+        opmlContent: _opmlContent
+    };
+});
