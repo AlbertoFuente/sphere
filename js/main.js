3c68598c5f98c5aecec0f8e62ab9195bac738a15
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-07-03 06:21:42 +0200

    change main.js folder and refactorized

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 require.config({
-    baseUrl: 'js',
     paths: {
         // modules
         'app': 'app',
@@ -8,9 +7,9 @@ require.config({
         'echoContent': 'echoContent',
         'opmlContent': 'opmlContent',
         // utils
-        'utils': 'utils/utils',
+        'utils': 'utils',
         // Plugins
-        'text': 'plugins/text',
+        'text': 'text',
         // Vendor
         'jquery': '../node_modules/jquery/dist/jquery.min',
         'materialize': '../node_modules/materialize-css/bin/materialize'
