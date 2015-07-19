99380757ab12c6b125650d9cc06b3b20577b9a7e
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-07-19 21:32:02 +0200

    Working on unit tests

--- tests/appSpec.js
+++ tests/appSpec.js
@@ -1,17 +1,38 @@
-define(['app'], function(app) {
+define(['app', 'topBar', 'utils'], function(app, topBar, utils) {
     'use strict';
 
     describe('Check App module', function() {
-        it('Init App', function() {
-            beforeEach(function() {
-                spyOn(app, 'init').andCallThrough();
-            });
+        var logo = utils._create('a'),
+            navContainer = utils._create('nav'),
+            options = utils._create('ul');
+        utils._setAttr(logo, 'class', 'brand-logo');
+        utils._setAttr(navContainer, 'id', 'menuBar');
+        utils._setAttr(options, 'id', 'nav-mobile');
+        utils._appendArr(window.document.body, [navContainer, logo, options]);
 
-            it("APP should be able to initialize", function() {
-                expect(app.init).toBeDefined();
-                app.init();
-                expect(app.init).toHaveBeenCalled();
-            });
+        it('Init App', function() {
+            expect(app.init).toBeDefined();
+            app.init();
+        });
+
+        it('Test topBar module', function() {
+            var menuOptions = {
+                op1: 'EchoJS',
+                op2: 'Javascript',
+                op3: 'DailyJS',
+                op5: 'jsRocks',
+                op6: 'FrontEndLabs',
+                op7: 'infoWorld',
+                op8: 'SitePoint',
+                op9: 'Telerik',
+                op10: 'cNet',
+                op11: 'infoQ'
+            };
+            // top bar
+            topBar.insertTitle('Sphere');
+            expect(logo.innerHTML).toEqual('Sphere');
+            //topBar.insertTitle.prototype.insertMenu(menuOptions);
+            //expect(options.childNodes.length).toBeGreaterThan(1);
         });
     });
 });
