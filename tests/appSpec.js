b1e26e7159a38e8faba93a4755a5f4d4fdb4ebba
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-07-20 10:45:15 +0200

    Working on unit tests

--- tests/appSpec.js
+++ tests/appSpec.js
@@ -4,19 +4,8 @@ define(['app', 'topBar', 'utils'], function(app, topBar, utils) {
     describe('Check App module', function() {
         var logo = utils._create('a'),
             navContainer = utils._create('nav'),
-            options = utils._create('ul');
-        utils._setAttr(logo, 'class', 'brand-logo');
-        utils._setAttr(navContainer, 'id', 'menuBar');
-        utils._setAttr(options, 'id', 'nav-mobile');
-        utils._appendArr(window.document.body, [navContainer, logo, options]);
-
-        it('Init App', function() {
-            expect(app.init).toBeDefined();
-            app.init();
-        });
-
-        it('Test topBar module', function() {
-            var menuOptions = {
+            options = utils._create('ul'),
+            menuOptions = {
                 op1: 'EchoJS',
                 op2: 'Javascript',
                 op3: 'DailyJS',
@@ -28,11 +17,25 @@ define(['app', 'topBar', 'utils'], function(app, topBar, utils) {
                 op10: 'cNet',
                 op11: 'infoQ'
             };
-            // top bar
+        utils._setAttr(logo, 'class', 'brand-logo');
+        utils._setAttr(navContainer, 'id', 'menuBar');
+        utils._setAttr(options, 'id', 'nav-mobile');
+        window.document.body.appendChild(navContainer);
+        utils._appendArr(navContainer, [logo, options]);
+
+        it('Init App', function() {
+            expect(app.init).toBeDefined();
+            app.init();
+        });
+
+        it('Test topBar module title', function() {
             topBar.insertTitle('Sphere');
             expect(logo.innerHTML).toEqual('Sphere');
-            //topBar.insertTitle.prototype.insertMenu(menuOptions);
-            //expect(options.childNodes.length).toBeGreaterThan(1);
+        });
+
+        it('Test topBar menu options', function() {
+            topBar.insertTitle.prototype.insertMenu(menuOptions);
+            expect(options.childNodes.length).toBeGreaterThan(1);
         });
     });
 });
